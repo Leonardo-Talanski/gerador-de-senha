64b65bbdebe5438f00d8c66aa3dd6b39553cf6d3
LeonardoTalanski <leonardo.e.t@hotmail.com>
2023-10-16 23:57:55 -0300

    criação variáveis slider,button,valor e password

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 
-let sliderElement = document.querySelector('#slider')
+let sliderElement = document.querySelector('#slider');
+let buttonElement = doucment.querySelector('#button');
+
+
+let sizePassword = querySelector('#valor');
+let password = document.querySelector('#password');
